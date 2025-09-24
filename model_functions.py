9bc914b6ff66f73b18998919e3affbe889a372b0
zeshanalvi <zeshankhanalvi@gmail.com>
2025-09-24 17:20:41 +0500

    Update model_functions.py

--- model_functions.py
+++ model_functions.py
@@ -7,7 +7,7 @@ import re
 from nltk.translate.meteor_score import meteor_score
 
 
-def forward_batch(images, input_ids, attention_mask, answers, question_classes=None,qtype_classifier=None,fusion_module=None,q_types=None,q_types_mapping=None,task_heads=None,device=None,image_encoder=None,question_encoder=None,disease_model=None):
+def forward_batch(images, input_ids, attention_mask, answers, question_classes=None,qtype_classifier=None,fusion_module=None,q_types=None,q_types_mapping=None,task_heads=None,device=None,image_encoder=None,question_encoder=None):
     # Image encoding
     img_outputs = image_encoder(pixel_values=images.to(device))
     img_feat = img_outputs.last_hidden_state  # [B, R, 768]
