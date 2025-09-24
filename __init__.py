d6c469240de96d195e7ff9ca677fca46ee5b836a
zeshanalvi <zeshankhanalvi@gmail.com>
2025-09-24 16:30:06 +0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,3 +1,4 @@
+"""
 from qtype import QuestionTypeClassifier
 from tpred import TaskPredictor
 from model import VQAModel
@@ -7,7 +8,7 @@ import torch
 import torch.nn as nn
 from datasets import load_dataset
 from torch.utils.data import DataLoader
-
+"""
 
 from .functions import preprocess_example, preprocess_image, collate_fn
 from .model import VQAModel
