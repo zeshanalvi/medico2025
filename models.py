553f22d19b0fafe52d087be8df915efbf21c2566
zeshanalvi <zeshankhanalvi@gmail.com>
2025-10-28 08:57:43 +0500

    Update models.py

--- models.py
+++ models.py
@@ -1,5 +1,9 @@
 import torch
 import torch.nn as nn
+import sys, os
+from git import Repo
+import os
+import numpy as np
 
 from transformers import (
     AutoTokenizer, AutoModelForSequenceClassification,
@@ -19,8 +23,7 @@ def generate_descriptive_answer(question, prediction, fused_features):
     outputs = gen_model.generate(**inputs, max_length=50)
     return gen_tokenizer.decode(outputs[0], skip_special_tokens=True)
 
-from git import Repo
-import os
+
 
 def clone_repo():
     repo_url = "https://github.com/zeshanalvi/Feature-Extraction.git"
@@ -42,45 +45,18 @@ def clone_repo():
 
 def diseasem(model,img):
     clone_repo()
-    import sys, os
+    from features import extract_features_batch
     repo_path = os.path.abspath("Feature-Extraction")
     if repo_path not in sys.path:
         sys.path.append(repo_path)
-    from features import extract_features_batch
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     res=extract_features_batch(img)
-    #from treenet import TreeNet
-    #model = TreeNet(layer_count=6, breath_count=3)
-    #model.load("Medico2025/treenet_model63.pkl")
     pred=model.predict_prob(res)
     disease_vec=torch.tensor(pred, dtype=torch.float32).to(device)
     return disease_vec
 
 def disease_model(img):
-    #!git clone https://github.com/zeshanalvi/Feature-Extraction.git
-    clone_repo()
-    import sys, os
-    repo_path = os.path.abspath("Feature-Extraction")
-    if repo_path not in sys.path:
-        sys.path.append(repo_path)
-    from features import extract_features_batch
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    res=extract_features_batch(img)
-    #print(type(res))
-    #print(res)
-    #!pip install dtreenetwork
-    from treenet import TreeNet
-    model = TreeNet(layer_count=6, breath_count=3)
-    model.load("Medico2025/treenet_model63.pkl")
-    pred=model.predict_prob(res)
-    #print(type(pred))
-    #print(pred)
-    #print(pred.shape)
-    disease_vec=torch.tensor(pred, dtype=torch.float32).to(device)
-    #print(type(disease_vec))
-    #print(disease_vec)
-    #return torch.tensor(np.random.rand(23)).to(device)
-    return disease_vec
+    return torch.tensor(np.random.rand(23)).to(device)
 
 router_name = "distilbert-base-uncased"
 router_tokenizer = AutoTokenizer.from_pretrained(router_name)
