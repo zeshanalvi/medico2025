ce932fb5b488c0751cbf03f4bbbb376a058b7cf9
zeshanalvi <zeshankhanalvi@gmail.com>
2025-11-10 09:34:23 +0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,14 +1,2 @@
-"""
-from qtype import QuestionTypeClassifier
-from tpred import TaskPredictor
-from model import VQAModel
-from fussionmodel import CoAttentionFusion
-from functions import preprocess_example, preprocess_image, collate_fn
-import torch
-import torch.nn as nn
-from datasets import load_dataset
-from torch.utils.data import DataLoader
-"""
-
 from .functions import preprocess_example, preprocess_image, collate_fn
 from .model import VQAModel
